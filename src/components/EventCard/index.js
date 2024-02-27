9489f15c4e3335b9123d083a8f22f609fd05abc1
2emeagauche <pychassaigne@gmail.com>
2024-02-27 15:13:25 +0100

    on supprime les proptype required et on met des valeurs par défaut pour éviter les warning du test

--- src/components/EventCard/index.js
+++ src/components/EventCard/index.js
@@ -29,17 +29,21 @@ const EventCard = ({
   );
 
 EventCard.propTypes = {
-  imageSrc: PropTypes.string.isRequired,
+  imageSrc: PropTypes.string,
   imageAlt: PropTypes.string,
-  date: PropTypes.instanceOf(Date).isRequired,
-  title: PropTypes.string.isRequired,
+  date: PropTypes.instanceOf(Date),
+  title: PropTypes.string,
   small: PropTypes.bool,
-  label: PropTypes.string.isRequired,
+  label: PropTypes.string,
 };
 
 EventCard.defaultProps = {
+  imageSrc: "",
   imageAlt: "image",
+  date: new Date().getTime(),
+  title: "événement",
   small: false,
+  label: "événement",
 }
 
 export default EventCard;
