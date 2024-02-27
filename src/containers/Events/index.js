788503c59336b90d1c8894488d316d5a6790e5ef
2emeagauche <pychassaigne@gmail.com>
2024-02-27 15:57:41 +0100

    La pagination ne s'affiche que si il y a au minimum 2 pages

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -62,14 +62,14 @@ const EventList = () => {
               </Modal>
             ))}
           </div>
-          <div className="Pagination">
+          {pageNumber >= 2 && <div className="Pagination">
             {[...Array(pageNumber || 0)].map((_, n) => (
               // eslint-disable-next-line react/no-array-index-key
               <a key={n} href="#events" onClick={() => setCurrentPage(n + 1)}>
                 {n + 1}
               </a>
             ))}
-          </div>
+          </div>}
         </>
       )}
     </>
