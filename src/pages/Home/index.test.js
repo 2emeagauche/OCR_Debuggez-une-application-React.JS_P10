434a5f89e0b2920383dbc14127ee44ccec0c7b9a
2emeagauche <pychassaigne@gmail.com>
2024-02-29 20:14:41 +0100

    ajout du test fonctionnel sur la home "a list of events is displayed"

--- src/pages/Home/index.test.js
+++ src/pages/Home/index.test.js
@@ -1,5 +1,42 @@
 import { fireEvent, render, screen } from "@testing-library/react";
+import { api, DataProvider } from "../../contexts/DataContext";
 import Home from "./index";
+
+
+const data = {
+  events: [
+    {
+      id: 1,
+      type: "soirée entreprise",
+      date: "2022-04-29T20:28:45.744Z",
+      title: "Conférence #productCON",
+      cover: "/images/stem-list-EVgsAbL51Rk-unsplash.png",
+      description:
+        "Présentation des outils analytics aux professionnels du secteur",
+      nb_guesses: 1300,
+      periode: "24-25-26 Février",
+      prestations: [
+        "1 espace d’exposition",
+        "1 scéne principale",
+        "2 espaces de restaurations",
+        "1 site web dédié",
+      ],
+    },
+
+    {
+      id: 2,
+      type: "forum",
+      date: "2022-04-29T20:28:45.744Z",
+      title: "Forum #productCON",
+      cover: "/images/stem-list-EVgsAbL51Rk-unsplash.png",
+      description:
+        "Présentation des outils analytics aux professionnels du secteur",
+      nb_guesses: 1300,
+      periode: "24-25-26 Février",
+      prestations: ["1 espace d’exposition", "1 scéne principale"],
+    },
+  ],
+};
 
 describe("When Form is created", () => {
   it("a list of fields card is displayed", async () => {
@@ -29,8 +66,13 @@ describe("When Form is created", () => {
 
 
 describe("When a page is created", () => {
-  it("a list of events is displayed", () => {
-    // to implement    
+  it("a list of events is displayed", async () => {
+    api.loadData = jest.fn().mockReturnValue(data);
+    render(<DataProvider>
+      <Home />
+    </DataProvider>)
+    // 2 events in mock data + 1 event in footer
+    expect((await screen.findAllByTestId("card-testid")).length).toEqual(3);
   })
   it("a list a people is displayed", () => {
     render(<Home />)
